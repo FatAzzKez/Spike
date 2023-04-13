867c197a913d34348098c53c6e66d9765ce5258a
arenk <aker0015@student.monash.edu>
2023-04-13 17:05:33 +1000

    Added openai dependency and made requests Figured out how to make requests to openai completion endpoint

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
 // npm run dev
 // currently prints my api key in the server after entering localhost:5000/api
+
+// some code to get my api key from another file. not commiting this code to repo lol
 const data=require('./apiKey.json') 
-const apiKey = data["apiKey"]
+const myApiKey = data["apiKey"]
 
-
+// don't know...
 const express = require('express')
 const app = express()
 const serverPort = 5000 // dependency in client/package.json proxy 
 
 app.get("/api", (req, res) => {
-    res.send(apiKey)
+    response = run()
+    console.log("I ran the program")
+    res.send("hi") 
 })
 
+
+//this code is all to do the request using openai api
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: myApiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+async function run() {
+    const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: "write me a 50 word essay on one piece the anime"}],
+    });
+     console.log(completion.data.choices[0].message.content);
+}
+
+// don't know what this is..
 app.listen(serverPort, () => {console.log(`server started on port ${serverPort}`)})
